1e324b5add0dcacf694ea98c111c39629bc78f70
Brian W. Wolter <brianwolter@gmail.com>
2022-03-14 10:35:19 -0700

    ...

--- src/error.rs
+++ src/error.rs
@@ -1,18 +1,34 @@
 use std::fmt;
+use std::num;
 
+use nom;
 use chrono;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum Error {
   NoSuchCommand(String),
   MissingArgument(String),
   ParseTimeError(chrono::format::ParseError),
+  ParseDurationError,
+  ParseIntError(num::ParseIntError),
   DurationRoundingError(chrono::RoundingError),
 }
 
 impl From<chrono::format::ParseError> for Error {
   fn from(error: chrono::format::ParseError) -> Self {
     Self::ParseTimeError(error)
+  }
+}
+
+impl<E> From<nom::Err<E>> for Error {
+  fn from(error: nom::Err<E>) -> Self {
+    Self::ParseDurationError
+  }
+}
+
+impl From<std::num::ParseIntError> for Error {
+  fn from(error: std::num::ParseIntError) -> Self {
+    Self::ParseIntError(error)
   }
 }
 
@@ -28,6 +44,8 @@ impl fmt::Display for Error {
       Self::NoSuchCommand(msg) => write!(f, "No such command: {}", msg),
       Self::MissingArgument(msg) => write!(f, "Missing argument: {}", msg),
       Self::ParseTimeError(err) => err.fmt(f),
+      Self::ParseDurationError => write!(f, "Invalid duration"),
+      Self::ParseIntError(err) => err.fmt(f),
       Self::DurationRoundingError(err) => err.fmt(f),
     }
   }
--- src/time.rs
+++ src/time.rs
@@ -1,3 +1,4 @@
+use nom;
 use chrono::{self, DurationRound};
 
 use crate::error;
@@ -16,4 +17,72 @@ pub fn parse_date(s: &str) -> Result<chrono::DateTime<chrono::Utc>, error::Error
   };
   let d = chrono::DateTime::parse_from_rfc3339(&s)?;
   Ok(d.into())
+}
+
+fn is_digit(c: char) -> bool {
+  c.is_digit(10)
+}
+
+fn is_unit(c: char) -> bool {
+  match c {
+    'h' | 'm' | 's' => true,
+    _ => false,
+  }
+}
+
+fn parse_value(input: &str) -> Result<i64, error::Error> {
+  Ok(i64::from_str_radix(input, 10)?)
+}
+
+fn value_primary(input: &str) -> nom::IResult<&str, i64> {
+  nom::combinator::map_res(
+    nom::bytes::complete::take_while_m_n(1, 10, is_digit),
+    parse_value
+  )(input)
+}
+
+fn parse_unit(input: &str) -> Result<i64, error::Error> {
+  match input {
+    "h" => Ok(60 * 60),
+    "m" => Ok(60),
+    "s" => Ok(1),
+    _   => Ok(0), // not representable
+  }
+}
+
+fn unit_primary(input: &str) -> nom::IResult<&str, i64> {
+  nom::combinator::map_res(
+    nom::bytes::complete::take_while_m_n(1, 1, is_unit),
+    parse_unit
+  )(input)
+}
+
+fn value_unit(input: &str) -> nom::IResult<&str, chrono::Duration> {
+  let (input, (val, unit)) = nom::sequence::tuple((value_primary, unit_primary))(input)?;
+  Ok((input, chrono::Duration::seconds(val * unit)))
+}
+
+pub fn parse_duration(input: &str) -> Result<chrono::Duration, error::Error> {
+  let mut result = chrono::Duration::seconds(0);
+  let mut input = input;
+  while input != "" {
+    let (remainder, duration) = value_unit(input)?;
+    result = result + duration;
+    input = remainder;
+  }
+  Ok(result)
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  
+  #[test]
+  fn test_parse_duration() {
+    assert_eq!(Ok(chrono::Duration::seconds(1)), parse_duration("1s"));
+    assert_eq!(Ok(chrono::Duration::seconds(2)), parse_duration("2s"));
+    assert_eq!(Ok(chrono::Duration::seconds(3600)), parse_duration("1h"));
+    assert_eq!(Ok(chrono::Duration::seconds(3660)), parse_duration("1h1m"));
+  }
+  
 }
